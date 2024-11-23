c42cfc1faa9e520d74614b137fd44e6be44fa943
dburdin <dbevziuk.tech@gmail.com>
2024-11-23 20:33:33 +0200

    improves related to images, also deploy

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,11 +1,10 @@
-import TestSvgrIcon from "/public/vite.svg?react"
+import TestSvgrIcon from "../assets/svg/vite.svg?react";
 
 export const App = () => {
   return (
     <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
-    <p>Teamplate</p>
-    <TestSvgrIcon />
+      <p>Teamplate</p>
+      <TestSvgrIcon />
     </div>
-    
   );
 };
