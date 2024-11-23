850c346fbcebf6e4634fd32fe37040ac94896094
dburdin <dbevziuk.tech@gmail.com>
2024-11-23 20:47:27 +0200

    Deploy check

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -3,7 +3,7 @@ import TestSvgrIcon from "../assets/svg/vite.svg?react";
 export const App = () => {
   return (
     <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
-      <p>Teamplate</p>
+      <p>Updated Teamplate</p>
       <TestSvgrIcon />
     </div>
   );
